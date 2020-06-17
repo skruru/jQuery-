b24d16ddad0a45fc4897be856003ad091a2ea78f
@skruru <s14k_ex-fw-myt@ezweb.ne.jp>
2020-06-17 11:25:52 +0900

    scroll_top

--- js/scroll_top.js
+++ js/scroll_top.js
@@ -1,22 +1,27 @@
 // DOMツリーが構築されたときに実行(他のJavascriptの読込も完了した後に実行します)
 $(function () {
   // トップへ戻るボタンのエレメントを取得
-  
+  const $scrollTop = $('.scroll-top');
   // ボタンをクリックしたらページトップまで戻るを実装
-  トップへ戻るボタンのエレメント.on('click', function () {
+  $scrollTop.on('click', function () {
     // スクロールの速度(ミリ秒)
-
+    const speed = 300;
     // スムーススクロールを実行
+    $('html, body').animate({scrollTop: 0}, speed, 'swing');
   });
 
   // windowのスクロール位置でボタンの表示・非表示
   $(window).on('scroll', function () {
     // スクロールの速度(ミリ秒)
-
+    const speed = 300;
     // 現在のスクロール位置を取得(windowのscrollTopを取得)
-
+    const $currentScroll = $(window).scrollTop();
     // もし現在のスクロール位置が30を超えていたらトップへ戻るボタンのエレメントをfadeInで表示
     // 30を超えていない場合はfadeOutで非表示
-
+    if ($currentScroll > 30){
+      $scrollTop.fadeIn();
+    } else {
+      $scrollTop.fadeOut();
+    }
   });
 });
