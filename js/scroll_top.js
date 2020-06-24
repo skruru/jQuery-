95c326a719284576ebc9ca6314f0e51fe45e1653
@skruru <s14k_ex-fw-myt@ezweb.ne.jp>
2020-06-24 16:47:54 +0900

    答え

--- js/scroll_top.js
+++ js/scroll_top.js
@@ -1,27 +1,34 @@
 // DOMツリーが構築されたときに実行(他のJavascriptの読込も完了した後に実行します)
 $(function () {
   // トップへ戻るボタンのエレメントを取得
-  const $scrollTop = $('.scroll-top');
+  // const $scrollTop = $('.scroll-top');
+  const $scrollTop = $('.scroll-top').first(); //最初のやつだけ取得
   // ボタンをクリックしたらページトップまで戻るを実装
   $scrollTop.on('click', function () {
     // スクロールの速度(ミリ秒)
-    const speed = 300;
+    const speed = 500;
     // スムーススクロールを実行
     $('html, body').animate({scrollTop: 0}, speed, 'swing');
   });
 
   // windowのスクロール位置でボタンの表示・非表示
   $(window).on('scroll', function () {
-    // スクロールの速度(ミリ秒)
-    const speed = 300;
+    // 表示の速度(ミリ秒)
+    const speed = 500;
     // 現在のスクロール位置を取得(windowのscrollTopを取得)
     const currentScroll = $(window).scrollTop();
     // もし現在のスクロール位置が30を超えていたらトップへ戻るボタンのエレメントをfadeInで表示
     // 30を超えていない場合はfadeOutで非表示
+    // if (currentScroll > 30){
+    //   $scrollTop.fadeIn(speed);
+    // } else {
+    //   $scrollTop.fadeOut(speed);
+    // }
+    //　同じ
     if (currentScroll > 30){
       $scrollTop.fadeIn(speed);
-    } else {
-      $scrollTop.fadeOut(speed);
+      return; //return false;　は最終手段
     }
+    $scrollTop.fadeOut(speed);
   });
 });
--- js/select.js
+++ js/select.js
@@ -6,24 +6,55 @@ $(function () {
   // 選択済みのセレクトボックス
   const $selected = $('#selected-items');
 
+  // 一つの関数でまとめる
+  const move = function($from, $to, $optionKey) {
+    const $options = $from.children('optionKey');
+    $options.prop('selected', false); //値を入れる時は基本prop
+    $to.appendTo($options);
+  };
+  // 関数を使うと。。。
+//    // 右に移動
+//    $('.js-item-to-right').on('click', function () {
+//     move($noneSelected, $selected, 'option:selected');
+//   });
+// ​
+//   // 右に全て移動
+//   $('.js-item-to-right-all').on('click', function () {
+//     move($noneSelected, $selected, 'option');
+//   });
+// ​
+//   // 左に移動
+//   $('.js-item-to-left').on('click', function () {
+//     move($selected, $noneSelected, 'option:selected');
+//   });
+// ​
+//   // 左に全て移動
+//   $('.js-item-to-left-all').on('click', function () {
+//     move($selected, $noneSelected, 'option');
+//   });
+
   // 右に移動
   $('.js-item-to-right').on('click', function () {
     // 選択されているoption(option:selected)を取得
-    let $optionSelected = $('#none-selected-items option:selected');
+    // let $optionSelected = $('#none-selected-items option:selected');
+    const $options = $noneSelected.children('option:selected');
     // 選択を解除
-    $optionSelected.prop('selected', false);
+    // $optionSelected.prop('selected', false);
+    $options.prop('selected', false); //値を入れる時は基本prop
     // 選択済みのセレクトボックスに移動
-    $optionSelected.appendTo($selected);
-
+    // $optionSelected.appendTo($selected);
+    $selected.appendTo($options);
     showBtn();
   });
 
   // 右に全て移動
   $('.js-item-to-right-all').on('click', function () {
     // 全てのoptionを取得
-    const $optionSelecteds = $('#none-selected-items > option');
+    // const $optionSelecteds = $('#none-selected-items > option');
+    const $options = $noneSelected.children('option');
     // 選択済みのセレクトボックスに移動
-    $optionSelecteds.appendTo($selected);
+    // $optionSelecteds.appendTo($selected);
+    $selected.append($options);
     showBtn();
   });
 
